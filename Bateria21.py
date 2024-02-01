3f5d8be76a4d66e0b2135953aa2a84d410b63e14
jvila97 <158268636+jvila97@users.noreply.github.com>
2024-02-01 09:49:19 +0100

    Create Bateria21.py

---
+++ Bateria21.py
@@ -0,0 +1,6 @@
+nom_usuari = input("Introdueix el teu nom: ")
+
+if nom_usuari == "Amilcar" or nom_usuari == "Sila":
+    print("Benvingut, " + nom_usuari + "!")
+else:
+    print("Nom d'usuari no vàlid. Torna-ho a provar.")
