b91f1d9ec972fe53f35e1b705ded9430b0d2fee9
jvila97 <158268636+jvila97@users.noreply.github.com>
2024-02-01 09:49:44 +0100

    Create Bateria22.py

---
+++ Bateria22.py
@@ -0,0 +1,8 @@
+numero = int(input("Introdueix un número: "))
+
+if numero > 100:
+    print("El número introduït és major que 100.")
+elif numero < 100:
+    print("El número introduït és menor que 100.")
+else:
+    print("El número introduït és igual a 100.")
