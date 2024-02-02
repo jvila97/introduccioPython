dbd28b3e2f57b388117fa8aa7cef0586bd2d5c06
jvila97 <158268636+jvila97@users.noreply.github.com>
2024-02-02 10:18:09 +0100

    Create bateria33.py

---
+++ bateria33.py
@@ -0,0 +1,8 @@
+num = int(input("Introdueix un número: "))
+suma = 0
+
+for i in range(1, num+1):
+    if i % 2 == 0:
+        suma += i
+
+print("La suma dels números parells fins a", num, "és:", suma)
